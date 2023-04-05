e4b97edb5daee027607b672c422e86b0bcb67acc
levOvchinnikov <lnrdpwr@gmail.com>
2023-04-05 16:59:00 +0500

    EnemyHealth

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private GameObject _effect;
+
+    public void DoDamage(int damage)
+    {
+        _health -= damage;
+        if(_health <= 0)
+        {
+            Instantiate(_effect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
