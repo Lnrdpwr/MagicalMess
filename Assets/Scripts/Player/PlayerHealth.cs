6e134cf8fdc503ed89d88449967228194eae6a2f
levOvchinnikov <lnrdpwr@gmail.com>
2023-04-06 19:32:41 +0500

    Player is invincible

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -8,17 +8,25 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private Image _healthBar;
     [SerializeField] private AnimationCurve _healthBarChangeCurve;
     [SerializeField] private float _timeToChangeBar;
+    [SerializeField] private float _invincibleTime;
 
     private float _currentHealth;
+    private SpriteRenderer _playerRenderer;
+    private bool _isInvincible;
 
     private void Start()
     {
         _currentHealth = _maximumHealth;
+        _playerRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void ChangeHealth(float changeAmount)//Отрицательное, если надо нанести урон
     {
-        _currentHealth += changeAmount;
+        if(changeAmount > 0 || !_isInvincible)
+        {
+            _currentHealth += changeAmount;
+        }
+        
         if(_currentHealth <= 0)
         {
             Destroy(gameObject);
@@ -26,6 +34,11 @@ public class PlayerHealth : MonoBehaviour
         else if(_currentHealth <= _maximumHealth)
         {
             StartCoroutine(ChangeBar(_currentHealth - changeAmount, changeAmount));
+            StartCoroutine(Invincible());
+        }
+        else
+        {
+            _currentHealth = _maximumHealth;
         }
     }
 
@@ -36,5 +49,19 @@ public class PlayerHealth : MonoBehaviour
             _healthBar.fillAmount = (changeFrom + _healthBarChangeCurve.Evaluate(i / _timeToChangeBar) * previousChange) / _maximumHealth;
             yield return new WaitForEndOfFrame();
         }
+    }
+
+    IEnumerator Invincible()
+    {
+        _isInvincible = true;
+        int alpha = 1;
+        for(float i = 0; i <= _invincibleTime; i += _invincibleTime / 5)
+        {
+            alpha = Mathf.Abs(alpha - 1);
+            _playerRenderer.color = new Color(1, 1, 1, alpha);
+            yield return new WaitForSeconds(_invincibleTime / 5);
+        }
+        _playerRenderer.color = new Color(1, 1, 1, 1);
+        _isInvincible = false;
     }
 }
