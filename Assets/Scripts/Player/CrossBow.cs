430133472b749a348b35d29721e76f334e0fcac2
Fosf0r4 <alex2007kuznetsov@gmail.com>
2023-04-06 20:06:31 +0300

    Added sprites and little changes

--- Assets/Scripts/Player/CrossBow.cs
+++ Assets/Scripts/Player/CrossBow.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 public class CrossBow : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
+    [SerializeField] private PlayerMovement _playerMovement;
 
-    //private Rigidbody2D _rb;
+    private Transform _transform;
+    private Rigidbody2D _rb;
     private Vector3 _mousePosition;
 
     public void Start()
     {
-        //_rb = GetComponent<Rigidbody2D>();
+        _transform = GetComponent<Transform>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -22,5 +25,18 @@ public class CrossBow : MonoBehaviour
         Vector3 lookDir = _mousePosition - transform.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        if (angle > -180f && angle < 0f)
+        {
+            _playerMovement.LookRight();
+            //_transform.position = new Vector3(0.15f,-0.15f,0);
+        }
+        else if (angle < -180f || angle > 0f)
+        {
+            _playerMovement.LookLeft();
+            //_transform.position = new Vector3(-0.15f, -0.15f,0);
+        }
+
+        Debug.Log(angle);
     }
 }
