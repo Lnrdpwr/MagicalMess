96c1d603fdbdfcbfb33346a0acad989ffc4e30e8
levOvchinnikov <lnrdpwr@gmail.com>
2023-04-06 19:55:31 +0500

    Bat prototype

--- Assets/Scripts/Enemies/Bat.cs
+++ Assets/Scripts/Enemies/Bat.cs
@@ -1,18 +1,48 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _maximumSpeed;
+    [SerializeField] private float _inactiveTime;
+    [SerializeField] private AnimationCurve _speedChange;
+
+    private EnemyAnimations _enemyAnimations;
+    private Rigidbody2D _enemyRigidbody;
+    private Transform _target;
+    private float _currentSpeed;
+
+    private void Start()
     {
-        
+        _currentSpeed = _maximumSpeed;
+        _enemyAnimations = GetComponent<EnemyAnimations>();
+        _enemyRigidbody = GetComponent<Rigidbody2D>();
+
+        _target = PlayerMovement.Instance.transform;
+        _enemyAnimations.SetMovingAnimation(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 direction = (_target.position - transform.position).normalized * _currentSpeed;
+        _enemyRigidbody.velocity = direction;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(Inactive());
+        }
+    }
+
+    IEnumerator Inactive()
+    {
+        for(float i = 0; i <= _inactiveTime; i += Time.deltaTime)
+        {
+            _currentSpeed = _speedChange.Evaluate(i / _inactiveTime) * _maximumSpeed;
+            yield return new WaitForEndOfFrame();
+        }
+        _currentSpeed = _maximumSpeed;
     }
 }
