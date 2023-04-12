f738506f44a2dd55b76c1ed4b29a083e1b603fd9
Fosf0r4 <alex2007kuznetsov@gmail.com>
2023-04-12 23:21:31 +0300

    Added WitchScythe (Need Fixes)

--- Assets/Scripts/Player/PlayerUsingSpells.cs
+++ Assets/Scripts/Player/PlayerUsingSpells.cs
@@ -4,10 +4,19 @@ public class PlayerUsingSpells : MonoBehaviour
 {
     [SerializeField] private Transform _firePoint;
     [SerializeField] private GameObject _fireBallPrefab;
+    [SerializeField] private GameObject _witchScythePrefab;
 
     private void UseElderScroll()
     {
         EldenScroll eldenScroll = Instantiate(_fireBallPrefab, _firePoint.position, _firePoint.parent.rotation).GetComponent<EldenScroll>();
+    }
+
+    private void UseWitchScythe()
+    {
+        WitchScythe witchScythe =
+            Instantiate(_witchScythePrefab, gameObject.transform.position, _witchScythePrefab.transform.rotation).GetComponent<WitchScythe>();
+
+        witchScythe.Parent = gameObject.transform;
     }
 
     public void Update()
@@ -15,6 +24,11 @@ public class PlayerUsingSpells : MonoBehaviour
         if (Input.GetButtonDown("Fire2"))
         {
             UseElderScroll();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            UseWitchScythe();
         }
     }
 }
