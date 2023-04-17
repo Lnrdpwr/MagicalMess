36e851ce00141abc68e1f5468dff6283d99557c5
Fosf0r4 <alex2007kuznetsov@gmail.com>
2023-04-17 19:30:22 +0300

    Added ReyCast

---
+++ Assets/Scripts/Menu/Interaction.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private PlayerShooting _playerShooting;
+
+    private void Update()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            if (hit.collider.tag == "ShopButton")
+            {
+                _playerShooting.CanShoot = false;
+            }
+        }
+    }
+}
