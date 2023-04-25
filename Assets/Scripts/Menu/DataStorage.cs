478019c9c74fe75b34b3e5eaf530c941492d99de
Brikka23 <ilyabogdanov787@gmail.com>
2023-04-25 20:22:11 +0300

    added choose device

---
+++ Assets/Scripts/Menu/DataStorage.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+
+public class DataStorage : MonoBehaviour
+{
+
+    void Start()
+    {
+
+        YandexGame.LoadCloud();
+    }
+
+    void Update()
+    {
+        
+    }
+}
